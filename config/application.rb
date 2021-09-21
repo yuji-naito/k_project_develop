006829e5412d71fd8ed3acd41dabe340befd065d
ynaito <naitya0412@gmail.com>
2021-09-21 13:10:22 +0900

    日本時刻対応

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Tokyo"
     config.i18n.default_locale = :ja
   end
 end
